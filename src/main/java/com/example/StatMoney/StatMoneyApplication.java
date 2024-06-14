5f284da6e48ec66c99d88860dac7cb079e91a904
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-06-14 15:07:18 +0700

    First commit

---
+++ src/main/java/com/example/StatMoney/StatMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.StatMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StatMoney/StatMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StatMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
